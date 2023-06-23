ad524dcc0853c85f4b98f6c95f1559ac6af9e843
vishalhandoo <81742803+vishalhandoo@users.noreply.github.com>
2023-06-23 20:06:24 +0530

    Add files via upload

---
+++ Lecture15Part1.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+
+    // char r,o,h,i,t;
+
+    // cin>>r>>o>>h>>i>>t;
+
+    
+
+    // char bname[10];
+    // cout<<"Enter the name : ";
+    // for(int i = 0 ; i < 6; i++){
+    //     cin>>bname[i];
+    // }
+    // cout<<"The name you have just entered is : "<<bname;
+
+    string name;
+    cin>>name;
+    cout<<name;
+
+    return 0;
+}
